8855c861b46cd13e6a7d35cddd394803a9e20eb9
PanosTsoukas <panaggiotis.tria@gmail.com>
2021-05-22 21:29:40 +0300

    Updates

---
+++ precache-manifest.2954f4100ad5890cbebcbf6ad4c5cfff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af02562a3808001aad1a7db90ff6143",
+    "url": "/Auction-Dapp/index.html"
+  },
+  {
+    "revision": "6c81d56e81273d2ac9b6",
+    "url": "/Auction-Dapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc3206f3f38302e80152",
+    "url": "/Auction-Dapp/static/js/2.362581ad.chunk.js"
+  },
+  {
+    "revision": "6c81d56e81273d2ac9b6",
+    "url": "/Auction-Dapp/static/js/main.f82309c4.chunk.js"
+  },
+  {
+    "revision": "2bee53e44d3713d1e1fc",
+    "url": "/Auction-Dapp/static/js/runtime-main.a08735cc.js"
+  }
+]);
